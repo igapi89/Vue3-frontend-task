6c07c64a1c1ce4f3501f4b5e57fff3cf2c64a9a4
igorapostolov <igor.apostolov@tatatu.com>
2023-06-15 16:28:11 +0200

    Adding new dish - Form submit & Form validation

--- app/src/components/DishForm.vue
+++ app/src/components/DishForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form :model="form" ref="formRef">
+  <el-form :model="form" ref="formRef" :rules="rules">
     <el-form-item label="Name" :label-width="formLabelWidth" prop="name">
       <el-input v-model="form.name" autocomplete="off" />
     </el-form-item>
@@ -57,13 +57,18 @@
     </el-form-item>
 
     <el-form-item>
-      <el-button type="primary"> Confirm </el-button>
+      <el-button type="primary" @click="submitForm(formRef)">
+        Confirm
+      </el-button>
+      <el-button @click="resetForm(formRef)">Reset</el-button>
     </el-form-item>
   </el-form>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from "vue";
+import { reactive, ref, toRaw } from "vue";
+import type { FormInstance, FormRules } from "element-plus";
+import { useStoreDishes } from "../stores/dishes";
 
 const props = defineProps({
   items: {
@@ -75,6 +80,7 @@ const props = defineProps({
 });
 
 const formLabelWidth = "140px";
+const formRef = ref<FormInstance>();
 
 // Form data
 let form = reactive(
@@ -90,6 +96,93 @@ let form = reactive(
         waitTime: null,
       }
 );
+
+// Custom validation rules for number type inputs
+const validatePrice = (rule: any, value: any, callback: any) => {
+  if (value == null) {
+    return callback(new Error("Please input dish price"));
+  } else {
+    if (value < 1) {
+      callback(new Error("Waiting time value cannot be below 1"));
+    } else {
+      callback();
+    }
+  }
+};
+
+const validateWaitTime = (rule: any, value: any, callback: any) => {
+  if (value == null) {
+    return callback(new Error("Please input dish waiting time"));
+  } else {
+    if (value < 5) {
+      callback(new Error("Waiting time value cannot be below 5"));
+    } else {
+      callback();
+    }
+  }
+};
+
+// Validation rules
+const rules = reactive<FormRules>({
+  name: [
+    { required: true, message: "Please input dish name", trigger: "change" },
+  ],
+  description: [
+    {
+      required: true,
+      message: "Please input dish description",
+      trigger: "change",
+    },
+  ],
+  price: [{ validator: validatePrice, required: true, trigger: "change" }],
+  category: [
+    {
+      required: true,
+      message: "Please select dish category",
+      trigger: "change",
+    },
+  ],
+  availability: [
+    {
+      required: true,
+      message: "Please select dish availability",
+      trigger: "change",
+    },
+  ],
+  waitTime: [
+    { validator: validateWaitTime, required: true, trigger: "change" },
+  ],
+});
+
+// Defining emit event for manipulating form dialog visibility
+const emit = defineEmits(["handleFormDialog"]);
+
+// Getting the dishes from store
+const dishesStore = useStoreDishes();
+
+// Form submit function
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      dishesStore.insertItem(toRaw(form));
+
+      emit("handleFormDialog", false);
+
+      resetForm(formEl);
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
+
+  dishesStore.getItems();
+};
+
+// Resetting form fields values
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.resetFields();
+};
 </script>
 
 <style scoped>
